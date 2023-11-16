91ce7a66ca7d2ed30e024b0cb8d4292d18e2e92c
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-11-16 14:45:10 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoMoreTimer
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public void Start()
+        {
+            var instance3 = new Harmony("JobMissionTimerPatch");
+            instance3.PatchAll(typeof(PatchJobMissionTimer));
+
+            Logger.LogInfo("Patched JobMissionTimer");
+        }
+    }
+
+    class PatchJobMissionTimer
+    {
+        [HarmonyPatch(typeof(JobMissionTimer), "OnServerUpdate")]
+        [HarmonyPrefix]
+        static bool OnServerUpdatePrefix(JobMissionTimer __instance)
+        {
+            JobMission jobMission = (JobMission)AccessTools.Field(typeof(JobMissionTimer), "jobMission").GetValue(__instance);
+            PlayerController[] players = GameObject.FindObjectsOfType<PlayerController>();
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (jobMission && __instance.onTimerFinishedShouldJobSuccess != null)
+                {
+                    jobMission.ServerJobCompleted(players[i]);
+                }
+            }
+
+            return false;
+        }
+    }
+}
